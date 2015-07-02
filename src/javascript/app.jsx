65ce11534390bc4128c1ce631a9bc1dca06da067
Emmanuel Morales <emmanuel.morales@headspring.com>
2015-07-02 08:08:03 -0500

    Simplify main application component

--- src/javascript/app.jsx
+++ src/javascript/app.jsx
@@ -1,26 +1,15 @@
 'use strict';
 import React from 'react';
-import {
-  run,
-  Route,
-  HashLocation,
-  DefaultRoute
-} from 'react-router';
-import App from './components/App.jsx';
 
-class Placeholder extends React.Component {
+class Test extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div className="jumbotron">
+        <h1>Hello, world!</h1>
+        <p>This is a simple Hello World application built on ReactJS (ES6) with Gulp</p>
+      </div>
     );
   }
 }
-var routes = (
-  <Route handler={App}>
-    <DefaultRoute handler={Placeholder}/>
-  </Route>
-);
 
-run(routes, HashLocation, (Root) => {
-  React.render(<Root/>, document.body);
-});
+React.render(<Test/>, document.body);
