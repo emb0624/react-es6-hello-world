e4ed784aadb5e376ec7c57f993950546351e4a4f
Emmanuel Morales <emmanuel.morales@headspring.com>
2015-07-09 20:37:03 -0500

    Update NPM package and Gulp tasks
    Updated NPM package.json to remove unused dependencies
    Updated Gulp tasks Clean to support callback
    Updated Gulp taks Default to support callback

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -2,7 +2,7 @@ var gulp 		= require('gulp');
 var config 	= require('../config').clean;
 var del 		= require('del');
 
-gulp.task('clean', function (cb) {
-  del(config.dest, cb);
+gulp.task('clean', function(cb) {
+  del([config.dest], cb);
 });
 
